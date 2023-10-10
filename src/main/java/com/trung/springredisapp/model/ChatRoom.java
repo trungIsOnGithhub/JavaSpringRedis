bf32a07a070be624334922c8986f9d6efeae7e2a
trungvuive <trung150502@gmail.com>
2023-10-10 09:55:11 +0700

    add list constructor for more than 2 user case

--- src/main/java/com/trung/springredisapp/model/ChatRoom.java
+++ src/main/java/com/trung/springredisapp/model/ChatRoom.java
@@ -3,6 +3,8 @@ package com.redisdeveloper.basicchat.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+
 @Getter
 @Setter
 public class Room {
@@ -12,13 +14,24 @@ public class Room {
     public Room(String id, String name) {
         this.id = id;
         this.names = new String[1];
+
         this.names[0] = name;
     }
 
     public Room(String id, String name1, String name2) {
         this.id = id;
         this.names = new String[2];
+
         this.names[0] = name1;
         this.names[1] = name2;
+    }
+
+    public Room(String id, ArrayList<String> names) {
+        this.id = id;
+        this.names = new String[names.size()];
+
+        for(int i=0; i<names.size(); ++i) {
+            this.names[i] = names[i];
+        }
     }
 }

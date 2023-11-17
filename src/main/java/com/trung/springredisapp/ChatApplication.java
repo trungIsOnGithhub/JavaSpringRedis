b8a97868ebc4bc75cf616f18f485482b71b61bf6
trungvuive <trung150502@gmail.com>
2023-11-17 13:52:25 +0700

    cors for local client running

--- src/main/java/com/trung/springredisapp/ChatApplication.java
+++ src/main/java/com/trung/springredisapp/ChatApplication.java
@@ -2,6 +2,9 @@ package com.trung.springredisapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Collections;
 
@@ -12,4 +15,14 @@ public class ChatApplication {
         app.setDefaultProperties(Collections.singletonMap("server.port", 8080));
         app.run(args);
     }
+
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowCredentials(true);
+			}
+		};
+	}
 }

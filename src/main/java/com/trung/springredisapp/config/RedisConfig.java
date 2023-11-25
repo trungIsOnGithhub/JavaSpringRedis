87b6db8cbafc875aa75797d2f367eae2a119743d
trungvuive <trung150502@gmail.com>
2023-11-25 12:39:30 +0700

    clearing redis configuration code

--- src/main/java/com/trung/springredisapp/config/RedisConfig.java
+++ src/main/java/com/trung/springredisapp/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.trung.springredisapp.config;
 
 import com.trung.springredisapp.redis.RedisMessageSubscriber;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +20,9 @@ import java.util.Objects;
 
 @Configuration
 public class RedisConfig {
+    @Autowired
+    CustomRedisApllicationProperties redisApplicationProperties;
+
     @Bean
     @ConditionalOnMissingBean(name = "redisTemplate")
     @Primary
@@ -45,30 +49,44 @@ public class RedisConfig {
         return new ChannelTopic("MESSAGES");
     }
 
+    // only has 1 connection a time in application
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        String host = null, port = null,
-                endpointUrl = null, password = null;
-
-        endpointUrl = System.getenv("REDIS_ENDPOINT_URL");
-        if(endpointUrl == null)
-            endpointUrl = "127.0.0.1:6379";
+        // endpointUrl = System.getenv("REDIS_ENDPOINT_URL");
+        // if(endpointUrl == null)
+        //     endpointUrl = "127.0.0.1:6379";
 
 
-        String[] urlParts = endpointUrl.split(":");
+        // String[] urlParts = endpointUrl.split(":");
 
-        if(urlParts.length > 1) {
-            host = urlParts[0];
-            port = urlParts[1];
+        // if(urlParts.length > 1) {
+        //     host = urlParts[0];
+        //     port = urlParts[1];
+        // }
+
+        // System.out.println(host + "|" + port);
+        RedisStandaloneConfiguration configuration = redisConfiguration();
+
+        // password = System.getenv("REDIS_PASSWORD");
+        // if( Objects.nonNull(password) )
+        //     config.setPassword(password);
+
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    RedisStandaloneConfiguration redisConfiguration() {
+        String host = redisApplicationProperties.getHost();
+        int port = redisApplicationProperties.getPort();
+
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
+
+        String password = redisApplicationProperties.getPassword();
+
+        if (password.length() > 0) {
+            configuration.setPassword(password);
         }
 
-        System.out.println(host + "|" + port);
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, Integer.parseInt(port));
-
-        password = System.getenv("REDIS_PASSWORD");
-        if( Objects.nonNull(password) )
-            config.setPassword(password);
-
-        return new LettuceConnectionFactory(config);
+        return configuration;
     }
 }
